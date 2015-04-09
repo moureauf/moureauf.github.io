c1da67c930a097383f2a2259fb7ceea806557b52
François Moureau <moureauf@gmail.com>
2015-04-09 18:39:27 +0300

    Back to normal

--- js/scale.fix.js
+++ js/scale.fix.js
@@ -3,7 +3,8 @@ var i;
 if (navigator.userAgent.match(/iPhone/i)) {
     for (i = 0; i < metas.length; i++) {
         if (metas[i].name == "viewport") {
-            metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+            metas[i].content =
+                "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
         }
     }
     document.addEventListener("gesturestart", gestureStart, false);
@@ -12,7 +13,8 @@ if (navigator.userAgent.match(/iPhone/i)) {
 function gestureStart() {
     for (i = 0; i < metas.length; i++) {
         if (metas[i].name == "viewport") {
-            metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+            metas[i].content =
+                "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
         }
     }
 }
